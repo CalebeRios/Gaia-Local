17507680934a2ad55656317610566e92f7a85cfa
Micaella <micaella2212@gmail.com>
2019-04-03 19:27:47 -0300

    Adding API connection
    Co-authored-by: Sofia Patrocínio <sofiapatrocinio@hotmail.com>
    Co-authored-by: Luís Taira <luishptaira@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 /* eslint-disable import/no-unresolved */
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const https = require('https');
 const app = express();
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.json({ Hello: 'World' });
+let data = '';
+let key = `yourkey`;
+let address = `brasilia`
+https.get(`https://api.opencagedata.com/geocode/v1/json?q=${address}&key=${key}`, (resp) => {
+  
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+  
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+  
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
 });
 
+
+app.get('/', (req, res) => {
+
+  pdata = JSON.parse(data);
+  res.json(pdata.results[1].geometry) ;
+});
 app.listen(3001);

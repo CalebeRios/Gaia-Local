41dd53f637c8ab94f86e99f11b80ec3f7107c5a1
Luis Taira <luishptaira@gmail.com>
2019-04-13 01:13:09 -0300

    Hotfixing stylesheet and codeclimate errors

--- src/server/requests/requestCoords.js
+++ src/server/requests/requestCoords.js
@@ -9,59 +9,33 @@ const locationSchema = new mongoose.Schema({
   longitude: String,
 });
 const Location = mongoose.model('Location', locationSchema);
-
+const newLocal = new Location({
+  name: '',
+  latitude: '',
+  longitude: '',
+});
 module.exports = {
   requestCoords: (theName) => {
-    const key = process.env.API_KEY;
-    this.key = key;
-
-    let data = '';
-    let body;
-    mongooseConnection.connect();
+    const key = process.env.API_KEY; this.key = key; let data = ''; let body; mongooseConnection.connect();
     return new Promise((resolve, reject) => {
-      if(theName == 'testLocal') {
+      if (theName === 'testLocal') {
         const testLocal = new Location({
-          name: theName,
-          latitude: 'test',
-          longitude: 'test',
+          name: theName, latitude: 'test', longitude: 'test',
         });
         resolve(testLocal);
       }
-      Location.findOne({ name: theName },
-        (err) => {
-          if (err) {
-            reject(err);
-          }
-        })
+      Location.findOne({ name: theName }, (err) => { if (err) { reject(err); } })
         .then((local) => {
-          if (local) {
-            resolve(local);
-          } else {
+          if (local) { resolve(local); } else {
             https.get(`https://api.opencagedata.com/geocode/v1/json?q=${theName}&key=${key}`, (resp) => {
-              resp.on('data', (chunk) => {
-                data += chunk;
+              resp.on('data', (chunk) => { data += chunk; }); resp.on('end', () => {
+                body = JSON.parse(data); if (body.results[0]) {
+                  newLocal.name = theName; newLocal.latitude = body.results[0].geometry.lat;
+                  newLocal.longitude = body.results[0].geometry.lng;
+                  newLocal.save((err) => { if (err) { reject(err); } }); resolve(newLocal);
+                } else { reject(new Error('value is undefined')); }
               });
-              resp.on('end', () => {
-                body = JSON.parse(data);
-                if (body.results[0]) {
-                  const newLocal = new Location({
-                    name: theName,
-                    latitude: body.results[0].geometry.lat,
-                    longitude: body.results[0].geometry.lng,
-                  });
-                  newLocal.save((err) => {
-                    if (err) {
-                      reject(err);
-                    }
-                  });
-                  resolve(newLocal);
-                } else {
-                  reject(new Error('value is undefined'));
-                }
-              });
-            }).on('error', (err) => {
-              reject(err);
-            });
+            }).on('error', (err) => { reject(err); });
           }
         });
     });
